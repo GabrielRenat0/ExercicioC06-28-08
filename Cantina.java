4eb994abba3cadd489b0e87eeecb3aa7982a920d
GabrielRenat0 <gabrielrenato382004@gmail.com>
2025-08-28 19:04:50 -0300

    Correção 1

--- Cantina.java
+++ Cantina.java
@@ -1,23 +1,31 @@
-import java.util.ArrayList;
-
 public class Cantina {
     public String nome;
-    private ArrayList<Salgado> salgados;
+    private Salgado[] salgados = new Salgado[10];
+    private int numSalgados = 0; 
     
     public Cantina(String nome) {
         this.nome = nome;
-        this.salgados = new ArrayList<>();
     }
     
     public void addSalgado(Salgado novoSalgado) {
-        salgados.add(novoSalgado);
+        if (numSalgados < salgados.length) {
+            salgados[numSalgados] = novoSalgado;
+            numSalgados++;
+        } else {
+            System.out.println("Não é possível adicionar mais salgados. Limite atingido.");
+        }
     }
     
     public void mostrainfo() {
         System.out.println("Cantina: " + nome);
-        System.out.println("Salgados disponíveis:");
-        for (int i = 0; i < salgados.size(); i++) {
-            System.out.println((i + 1) + ". " + salgados.get(i).nome);
+        System.out.println("Salgados disponíveis (" + numSalgados + "):");
+        
+        if (numSalgados == 0) {
+            System.out.println("Nenhum salgado cadastrado");
+        } else {
+            for (int i = 0; i < numSalgados; i++) {
+                System.out.println((i + 1) + ". " + salgados[i].nome);
+            }
         }
     }
 }
--- Main.java
+++ Main.java
@@ -1,14 +1,31 @@
 public class Main {
     public static void main(String[] args) {
         Cantina cantina = new Cantina("Cantina do Inatel");
-
+        
+        // Criar 3 salgados da escolha
         Salgado salgado1 = new Salgado("Coxinha");
         Salgado salgado2 = new Salgado("Pastel");
         Salgado salgado3 = new Salgado("Empada");
-
+        Salgado salgado4 = new Salgado("Esfiha");
+        Salgado salgado5 = new Salgado("Kibe");
         cantina.addSalgado(salgado1);
         cantina.addSalgado(salgado2);
         cantina.addSalgado(salgado3);
+        cantina.addSalgado(salgado4);
+        cantina.addSalgado(salgado5);
+
+        Salgado salgadoExtra = new Salgado("Pão de Queijo");
+        cantina.addSalgado(salgadoExtra);
+        Salgado salgadoExtra2 = new Salgado("Bolinha de Queijo");
+        cantina.addSalgado(salgadoExtra2);
+        Salgado salgadoExtra3 = new Salgado("Risole");
+        cantina.addSalgado(salgadoExtra3);
+        Salgado salgadoExtra4 = new Salgado("Enroladinho");
+        cantina.addSalgado(salgadoExtra4);
+        Salgado salgadoExtra5 = new Salgado("Croissant");
+        cantina.addSalgado(salgadoExtra5);
+        Salgado salgadoExtra6 = new Salgado("Torta"); 
+        cantina.addSalgado(salgadoExtra6);
 
         cantina.mostrainfo();
     }
